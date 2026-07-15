c1f0126884a7bf0e0d6a71f59bc31a6caf632b29
Karolina <karolina.santos@undf.edu.br>
2026-07-15 20:03:44 -0300

    Alteração na tabela autores

--- scripts/01_criacao_banco_tabelas.sql
+++ scripts/01_criacao_banco_tabelas.sql
@@ -1,9 +1,25 @@
+/* =====================================================
+   PROJETO FINAL - HPGTI
+   Sistema: Livraria
+   Arquivo: 01_criacao_banco_tabelas.sql
+===================================================== */
+
+/* Criação do banco de dados */
 create database LivrariaProjetoFinal;
 
+/* Seleciona o banco de dados */
 USE LivrariaProjetoFinal;
 
+/* =====================================================
+   TABELA: AUTORES
+   ================================================== */
 CREATE TABLE autores (
-	id_autor INT,
-	nome VARCHAR(100),
-	nacionalidade VARCHAR(50)
+    id_autor INT AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    nacionalidade VARCHAR(50) NOT NULL,
+
+    PRIMARY KEY (id_autor)
 );
+
+
+
